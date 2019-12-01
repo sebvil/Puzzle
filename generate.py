84ae3f62531c3a372319486242a3ead01ed9ff01
sebvil <sebasvime@gmail.com>
2019-11-30 22:31:24 -0600

    Some work done

---
+++ generate.py
@@ -0,0 +1,96 @@
+letters = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+]
+def generate_letter_button(letter, r, c, offset):
+    size = 30
+    y = size * r + 200
+    x = size * c + 100 + offset
+    s = letter.lower()
+    button = """
+        self.%s = tk.Button(self.master)
+        self.%s["text"] = "%s"
+        self.%s["command"] = self.print_%s
+        self.%s.place(x=%i, y=%i, width=%i, height=%i)
+    """ % (s, s, letter, s, s, s, x, y, size, size)
+    return button
+
+def generate_letter_function(letter):
+    s = letter.lower()
+    letter_function = """
+    def print_%s(self):
+        self.entry.insert(tk.INSERT, "%s")
+        self.wrong.place(x = 120, y =-50, height=50, width=240)
+
+    """ % (s, letter)
+    return letter_function
+
+size = 30
+y = 200 + size
+x = size * len(letters[1]) + 100 + 12
+s =  """
+import tkinter as tk
+
+class AnswerPage(tk.Frame):
+    def __init__(self, answer, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.answer = answer.upper()
+
+    def create_widgets(self):
+        self.entry = tk.Entry(self.master)
+        self.entry.place(x = 140, y = 100, width=200, height=30)
+
+        self.wrong = tk.Label(self.master, text="Wrong Answer")
+        self.wrong.place(x = 120, y = -50, height=50, width=240)
+
+        self.enter = tk.Button(self.master)
+        self.enter["text"] = "SUBMIT"
+        self.enter["command"] = self.submit
+        self.enter.place(x=140, y=150, width=200, height=30)
+
+        self.backspace = tk.Button(self.master)
+        self.backspace["text"] = "DELETE"
+        self.backspace["command"] = self.delete
+        self.backspace.place(x=%i, y=%i, width=%i, height=%i)
+""" % (x, y, size * 2, size)
+row = 0
+column = 0
+offset = 0
+for i in letters:
+    for j in i:
+        s += generate_letter_button(j,row, column, offset)
+        column += 1
+    if row == 0:
+        offset = 12
+    if row == 1:
+        offset = 40
+    row += 1
+    column = 0
+a = ord("A")
+for i in range(26):
+    s += generate_letter_function(chr(i + a))
+s += """
+    def submit(self):
+        attempt = self.entry.get()
+        if attempt == self.answer:
+            print("CORRECT")
+            self.quit()
+        else:
+            self.wrong.place(x = 120, y = 50, height=50, width=240)
+            print(1)
+
+    def delete(self):
+        index = self.entry.index(tk.INSERT)
+        self.entry.delete(index -1)
+
+
+"""
+
+
+f = open("answer_page.py", 'w')
+f.write(s)
+f.close()
---
+++ text_page.py
@@ -0,0 +1,243 @@
+import tkinter as tk
+
+class TextPage(tk.Frame):
+    def __init__(self, answer, master=None, next_page=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.answer = answer.upper()
+        self.next_page = next_page
+
+
+    def create_widgets(self):
+        self.entry = tk.Entry(self.master)
+        self.entry.place(x = 140, y = 100, width=200, height=30)
+
+        self.enter = tk.Button(self.master)
+        self.enter["text"] = "SUBMIT"
+        self.enter["command"] = self.submit
+        self.enter.place(x=140, y=150, width=200, height=30)
+
+        self.backspace = tk.Button(self.master)
+        self.backspace["text"] = "DELETE"
+        self.backspace["command"] = self.delete
+        self.backspace.place(x=382, y=230, width=60, height=30)
+
+        self.q = tk.Button(self.master)
+        self.q["text"] = "Q"
+        self.q["command"] = self.print_q
+        self.q.place(x=100, y=200, width=30, height=30)
+
+        self.w = tk.Button(self.master)
+        self.w["text"] = "W"
+        self.w["command"] = self.print_w
+        self.w.place(x=130, y=200, width=30, height=30)
+
+        self.e = tk.Button(self.master)
+        self.e["text"] = "E"
+        self.e["command"] = self.print_e
+        self.e.place(x=160, y=200, width=30, height=30)
+
+        self.r = tk.Button(self.master)
+        self.r["text"] = "R"
+        self.r["command"] = self.print_r
+        self.r.place(x=190, y=200, width=30, height=30)
+
+        self.t = tk.Button(self.master)
+        self.t["text"] = "T"
+        self.t["command"] = self.print_t
+        self.t.place(x=220, y=200, width=30, height=30)
+
+        self.y = tk.Button(self.master)
+        self.y["text"] = "Y"
+        self.y["command"] = self.print_y
+        self.y.place(x=250, y=200, width=30, height=30)
+
+        self.u = tk.Button(self.master)
+        self.u["text"] = "U"
+        self.u["command"] = self.print_u
+        self.u.place(x=280, y=200, width=30, height=30)
+
+        self.i = tk.Button(self.master)
+        self.i["text"] = "I"
+        self.i["command"] = self.print_i
+        self.i.place(x=310, y=200, width=30, height=30)
+
+        self.o = tk.Button(self.master)
+        self.o["text"] = "O"
+        self.o["command"] = self.print_o
+        self.o.place(x=340, y=200, width=30, height=30)
+
+        self.p = tk.Button(self.master)
+        self.p["text"] = "P"
+        self.p["command"] = self.print_p
+        self.p.place(x=370, y=200, width=30, height=30)
+
+        self.a = tk.Button(self.master)
+        self.a["text"] = "A"
+        self.a["command"] = self.print_a
+        self.a.place(x=112, y=230, width=30, height=30)
+
+        self.s = tk.Button(self.master)
+        self.s["text"] = "S"
+        self.s["command"] = self.print_s
+        self.s.place(x=142, y=230, width=30, height=30)
+
+        self.d = tk.Button(self.master)
+        self.d["text"] = "D"
+        self.d["command"] = self.print_d
+        self.d.place(x=172, y=230, width=30, height=30)
+
+        self.f = tk.Button(self.master)
+        self.f["text"] = "F"
+        self.f["command"] = self.print_f
+        self.f.place(x=202, y=230, width=30, height=30)
+
+        self.g = tk.Button(self.master)
+        self.g["text"] = "G"
+        self.g["command"] = self.print_g
+        self.g.place(x=232, y=230, width=30, height=30)
+
+        self.h = tk.Button(self.master)
+        self.h["text"] = "H"
+        self.h["command"] = self.print_h
+        self.h.place(x=262, y=230, width=30, height=30)
+
+        self.j = tk.Button(self.master)
+        self.j["text"] = "J"
+        self.j["command"] = self.print_j
+        self.j.place(x=292, y=230, width=30, height=30)
+
+        self.k = tk.Button(self.master)
+        self.k["text"] = "K"
+        self.k["command"] = self.print_k
+        self.k.place(x=322, y=230, width=30, height=30)
+
+        self.l = tk.Button(self.master)
+        self.l["text"] = "L"
+        self.l["command"] = self.print_l
+        self.l.place(x=352, y=230, width=30, height=30)
+
+        self.z = tk.Button(self.master)
+        self.z["text"] = "Z"
+        self.z["command"] = self.print_z
+        self.z.place(x=140, y=260, width=30, height=30)
+
+        self.x = tk.Button(self.master)
+        self.x["text"] = "X"
+        self.x["command"] = self.print_x
+        self.x.place(x=170, y=260, width=30, height=30)
+
+        self.c = tk.Button(self.master)
+        self.c["text"] = "C"
+        self.c["command"] = self.print_c
+        self.c.place(x=200, y=260, width=30, height=30)
+
+        self.v = tk.Button(self.master)
+        self.v["text"] = "V"
+        self.v["command"] = self.print_v
+        self.v.place(x=230, y=260, width=30, height=30)
+
+        self.b = tk.Button(self.master)
+        self.b["text"] = "B"
+        self.b["command"] = self.print_b
+        self.b.place(x=260, y=260, width=30, height=30)
+
+        self.n = tk.Button(self.master)
+        self.n["text"] = "N"
+        self.n["command"] = self.print_n
+        self.n.place(x=290, y=260, width=30, height=30)
+
+        self.m = tk.Button(self.master)
+        self.m["text"] = "M"
+        self.m["command"] = self.print_m
+        self.m.place(x=320, y=260, width=30, height=30)
+
+    def print_a(self):
+        self.entry.insert(tk.INSERT, "A")
+
+    def print_b(self):
+        self.entry.insert(tk.INSERT, "B")
+
+    def print_c(self):
+        self.entry.insert(tk.INSERT, "C")
+
+    def print_d(self):
+        self.entry.insert(tk.INSERT, "D")
+
+    def print_e(self):
+        self.entry.insert(tk.INSERT, "E")
+
+    def print_f(self):
+        self.entry.insert(tk.INSERT, "F")
+
+    def print_g(self):
+        self.entry.insert(tk.INSERT, "G")
+
+    def print_h(self):
+        self.entry.insert(tk.INSERT, "H")
+
+    def print_i(self):
+        self.entry.insert(tk.INSERT, "I")
+
+    def print_j(self):
+        self.entry.insert(tk.INSERT, "J")
+
+    def print_k(self):
+        self.entry.insert(tk.INSERT, "K")
+
+    def print_l(self):
+        self.entry.insert(tk.INSERT, "L")
+
+    def print_m(self):
+        self.entry.insert(tk.INSERT, "M")
+
+    def print_n(self):
+        self.entry.insert(tk.INSERT, "N")
+
+    def print_o(self):
+        self.entry.insert(tk.INSERT, "O")
+
+    def print_p(self):
+        self.entry.insert(tk.INSERT, "P")
+
+    def print_q(self):
+        self.entry.insert(tk.INSERT, "Q")
+
+    def print_r(self):
+        self.entry.insert(tk.INSERT, "R")
+
+    def print_s(self):
+        self.entry.insert(tk.INSERT, "S")
+
+    def print_t(self):
+        self.entry.insert(tk.INSERT, "T")
+
+    def print_u(self):
+        self.entry.insert(tk.INSERT, "U")
+
+    def print_v(self):
+        self.entry.insert(tk.INSERT, "V")
+
+    def print_w(self):
+        self.entry.insert(tk.INSERT, "W")
+
+    def print_x(self):
+        self.entry.insert(tk.INSERT, "X")
+
+    def print_y(self):
+        self.entry.insert(tk.INSERT, "Y")
+
+    def print_z(self):
+        self.entry.insert(tk.INSERT, "Z")
+
+    def submit(self):
+        attempt = self.entry.get()
+        if attempt == self.answer:
+            print("CORRECT")
+            self.quit()
+
+    def delete(self):
+        index = self.entry.index(tk.INSERT)
+        self.entry.delete(index -1)
